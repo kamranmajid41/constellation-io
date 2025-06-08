a57728919586a5872a46ad6256dc6b47b12f3f83
mynamesjeff1298j9ifj2 <sprioneinc@gmail.com>
2025-06-08 13:07:35 -0400

    adding the logo to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { AppShell, Burger, Group, Space, Text } from '@mantine/core';
 import '@mantine/core/styles.css';
-import { AppShell, Group, Text, Card, Burger, Skeleton, Title, Space} from '@mantine/core';
-import Globe from './components/Globe'; 
-import Settings from './components/Settings'; 
-import ActiveCard from './components/ActiveCard';
 import { useDisclosure } from '@mantine/hooks';
-
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { useEffect, useState } from 'react';
+import ActiveCard from './components/ActiveCard';
+import Globe from './components/Globe';
+import Settings from './components/Settings';
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
@@ -22,91 +21,83 @@ function App() {
         setUtcTime(new Date().toUTCString().split(' ')[4]);
       };
       const intervalId = setInterval(updateClock, 1000);
-      updateClock(); // Initial call
+      updateClock(); 
       return () => clearInterval(intervalId);
     }, []);
 
-    return <Text 
-              c='green' component="span" size="xs" weight={200} 
-              style={{left: 6, position: 'absolute'}}
-            >
-                {utcTime}
-           </Text>;
+    return (
+      <Text
+        c="green"
+        size="xs"
+        weight={200}
+        style={{
+          marginLeft: '-8px',
+          marginTop: '2px',
+          textAlign: 'left',
+          display: 'block',
+          marginBottom: '2px'
+        }}
+      >
+        {utcTime}
+      </Text>
+    );
   };
 
   return (
     <>
-    <AppShell
-      navbar={{ width: 66, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-    >
-      <AppShell.Navbar p="md">
-        <Group>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <UTCClock />
-        </Group>
-        <Space h='15pt'/>
-    
-        <Settings
-          setCustomTleData={setCustomTleData}
-          setFlightTrajectoryData={setFlightTrajectoryData}
-          setActivePanel={setActivePanel}
-          activePanel={activePanel}
-        />
-     
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <Globe/>
-        <ActiveCard
-          activePanel={activePanel}
-          setActivePanel={setActivePanel}
-        />
-      </AppShell.Main>
-    </AppShell>
+      <AppShell
+        navbar={{ width: 66, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      >
+        <AppShell.Navbar p="md">
+          <Group
+            gap={8}
+            style={{
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              paddingLeft: '2px',
+              width: '100%',
+            }}
+          >
+            <img
+              src="/constellation-io/2018Lebron.png"
+              alt="LeBron Icon"
+              style={{
+                width: '52px',             
+                height: '52px',
+                objectFit: 'contain',
+                display: 'block',
+                marginLeft: '-8px',
+                marginBottom: '0px'
+              }}
+            />
+            <UTCClock />
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
+            />
+          </Group>
+
+          <Space h='12pt'/>
+          <Settings
+            setCustomTleData={setCustomTleData}
+            setFlightTrajectoryData={setFlightTrajectoryData}
+            setActivePanel={setActivePanel}
+            activePanel={activePanel}
+          />
+        </AppShell.Navbar>
+
+        <AppShell.Main>
+          <Globe />
+          <ActiveCard
+            activePanel={activePanel}
+            setActivePanel={setActivePanel}
+          />
+        </AppShell.Main>
+      </AppShell>
     </>
   );
 }
-
-  // <Button
-  //           onClick={resetMapView}
-  //           variant="filled"
-  //           size="lg"
-  //           style={{
-  //             position: 'absolute',
-  //             top: 5,
-  //             left: 0,
-  //             zIndex: 10,
-  //             padding: '10px 20px',
-  //             fontFamily: 'Courier New, Courier, monospace',
-  //             fontSize: '18px',
-  //             fontWeight: 'light',
-  //             backgroundColor: '#000',
-  //             color: '#fff',
-  //           }}
-  //         >
-  //           constellation-io
-  //         </Button>
-
-  //         <Settings
-  //           setCustomTleData={setCustomTleData}
-  //           setFlightTrajectoryData={setFlightTrajectoryData}
-  //         />
-
-  //         <Button
-  //           style={{
-  //               position: 'absolute',
-  //               bottom: 0,
-  //               left: 0,
-  //               zIndex: 100,
-  //               backgroundColor: '#000',
-  //               color: '#fff',
-  //             }}
-  //         >
-  //           <Group position="apart" align="center" mb="sm">
-  //             <IconClockHour4 size={15} color='grey'/>
-  //             <Text size="sm" weight={200}>
-  //               <UTCClock />
-  //             </Text>
-  //           </Group>
-  //         </Button>
 
 export default App;
